a748ab7a2c21391e7c82a95843047ef3d71c3ed2
haokaili <>
2021-05-27 22:41:06 +0000

    finish

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ICS2O-Unit5-05-GO-Radio-Button
+[![GitHub's Super Linter](https://github.com/haokai-li/ICS2O-Unit5-05-GO-Radio-Button/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/haokai-li/ICS2O-Unit5-05-GO-Radio-Button/actions)
---
+++ weekInput.go
@@ -0,0 +1,38 @@
+// Created by: haokai
+// Created on: May 2021
+// This program displays, "Radio-Button"
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// This function does addition
+	var age int
+	var day string
+
+	// input
+	fmt.Println("This program is about age limit of movie .")
+	fmt.Println()
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
+	fmt.Print("Enter the day of the week: ")
+	fmt.Scanln(&day)
+
+	// detect
+	if (day == "saturday" || day == "sunday") && (age >= 18 || age < 18) {
+		// relax
+		fmt.Println("Time to relax for the weekend!")
+	} else if !(day == "tuesday" || day == "thursday") && age < 18 {
+		// go to school
+		fmt.Println("Time for school!")
+	} else if !(day == "tuesday" || day == "thursday") && age >= 18 {
+		// go to work
+		fmt.Println("Time to go to work!")
+	}
+
+	fmt.Println("\nDone.")
+}
